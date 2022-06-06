0646881b208b5d0fe0b5c3ba4bfd22408039ee31
Zoquinha123 <gilson.morais@fatec.sp.gov.br>
2022-06-06 18:25:23 +0000

    gravando

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa{
+    private String nome;
+    private String endereco;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+
+    public void setEndereco(String endereco) {
+            this.endereco = endereco;
+
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    
+    }
+}
---
+++ PessoaFisica.java
@@ -0,0 +1,10 @@
+public class PessoaFisica extends Pessoa{
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpd (String cpf) {
+        this.cpf = cpf;
+    }
+}
